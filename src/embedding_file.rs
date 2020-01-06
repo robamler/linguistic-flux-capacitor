cbd6f580f909cd008448e3d341295dea52215f99
Robert Bamler <robert.bamler@gmail.com>
2020-01-05 16:07:17 -0800

    Make memory mapped file own its data

--- src/embedding_file.rs
+++ src/embedding_file.rs
@@ -1,14 +1,12 @@
 use crate::ans::{Decoder, DistributionU8};
 
+use wasm_bindgen::prelude::*;
+
 use std::convert::TryInto;
 
-#[repr(C)]
-struct EmbeddingFile<'a> {
-    pub header: &'a FileHeader,
-    pub timestep_addrs: &'a [u32],
-    pub first_embeddings: &'a [i8],
-    pub last_embeddings: &'a [i8],
-    raw_data: &'a [u32],
+#[wasm_bindgen]
+struct EmbeddingFile {
+    raw_data: Box<[u32]>,
 }
 
 #[repr(C)]
@@ -30,24 +28,20 @@ struct Timestep<'a> {
     raw_data: &'a [u32],
 }
 
-impl<'a> EmbeddingFile<'a> {
-    pub fn new(data: &'a [u32]) -> Result<Self, ()> {
-        const HEADER_SIZE: usize = std::mem::size_of::<FileHeader>() / 4;
-        if data.len() < HEADER_SIZE {
+impl EmbeddingFile {
+    const HEADER_SIZE: usize = std::mem::size_of::<FileHeader>() / 4;
+
+    pub fn new(data: Box<[u32]>) -> Result<Self, ()> {
+        if data.len() < Self::HEADER_SIZE {
             return Err(());
         }
 
-        let header_array: [u32; HEADER_SIZE] = data[0..HEADER_SIZE].try_into().unwrap();
-        let header = unsafe {
-            // This is safe because `FileHeader` is `repr(C)` and has the same alignment as
-            // `[u32; HEADER_SIZE]
-            &*(&header_array as *const [u32; HEADER_SIZE] as *const FileHeader)
-        };
-
+        let file = Self { raw_data: data };
+        let header = file.header();
         let embeddings_size = header.vocab_size * header.embedding_dim;
 
         if header.major_version != 0
-            || header.file_size as usize != data.len()
+            || header.file_size as usize != file.raw_data.len()
             || header.num_timesteps < 2
             || embeddings_size == 0
             || embeddings_size % 4 != 0
@@ -57,7 +51,7 @@ impl<'a> EmbeddingFile<'a> {
             return Err(());
         }
 
-        let first_embeddings_offset = HEADER_SIZE as u32 + header.num_timesteps - 2;
+        let first_embeddings_offset = Self::HEADER_SIZE as u32 + header.num_timesteps - 2;
         let last_embeddings_offset = first_embeddings_offset + embeddings_size / 4;
         let payload_offset = last_embeddings_offset + embeddings_size / 4;
 
@@ -65,32 +59,71 @@ impl<'a> EmbeddingFile<'a> {
             return Err(());
         }
 
-        // The below `unsafe` blocks are save because they just transmute
-        // from `&[u32]` to `&[i8]`.
-        Ok(EmbeddingFile {
-            header,
-            timestep_addrs: &data[HEADER_SIZE..first_embeddings_offset as usize],
-            first_embeddings: get_i8_slice(
-                &data[first_embeddings_offset as usize..last_embeddings_offset as usize],
-            ),
-            last_embeddings: get_i8_slice(
-                &data[last_embeddings_offset as usize..payload_offset as usize],
-            ),
-            raw_data: &data[payload_offset as usize..],
+        Ok(file)
+    }
+
+    pub fn header(&self) -> &FileHeader {
+        let header_slice = unsafe {
+            // This is safe because the constructor checks that `raw_data` is big enough.
+            self.raw_data.get_unchecked(0..Self::HEADER_SIZE)
+        };
+        let header_array: [u32; Self::HEADER_SIZE] = header_slice.try_into().unwrap();
+        unsafe {
+            // This is safe because `FileHeader` is `repr(C)` and has the same alignment as
+            // `[u32; HEADER_SIZE]
+            &*(&header_array as *const [u32; Self::HEADER_SIZE] as *const FileHeader)
+        }
+    }
+
+    pub fn first_embeddings(&self) -> &[i8] {
+        let header = self.header();
+        let begin = Self::HEADER_SIZE as u32 + header.num_timesteps - 2;
+        let end = begin + header.vocab_size * header.embedding_dim / 4;
+
+        get_i8_slice(unsafe {
+            // This is safe because the constructor checks that `raw_data` is big enough.
+            &self.raw_data.get_unchecked(begin as usize..end as usize)
         })
     }
 
-    fn timestep(&self, t: u32) -> Result<Timestep<'a>, ()> {
-        if t == 0 || t > self.header.num_timesteps {
+    pub fn last_embeddings(&self) -> &[i8] {
+        let header = self.header();
+        let embedding_size = header.vocab_size * header.embedding_dim / 4;
+        let begin = Self::HEADER_SIZE as u32 + header.num_timesteps - 2 + embedding_size;
+        let end = begin + embedding_size;
+
+        get_i8_slice(unsafe {
+            // This is safe because the constructor checks that `raw_data` is big enough.
+            &self.raw_data.get_unchecked(begin as usize..end as usize)
+        })
+    }
+
+    pub fn timestep(&self, t: u32) -> Result<Timestep, ()> {
+        if t == 0 || t > self.header().num_timesteps {
             Err(())
         } else {
-            Timestep::new(self.raw_data, self.timestep_addrs[t as usize - 1])
+            let addr = unsafe {
+                // This is safe because the constructor checks that `raw_data` is big enough.
+                *self
+                    .raw_data
+                    .get_unchecked(Self::HEADER_SIZE + (t - 1) as usize)
+            };
+
+            let header = self.header();
+            // `vocab_size` is guaranteed to be a multiple of `chunk_size`.
+            let num_chunks = header.vocab_size / header.chunk_size;
+
+            Timestep::new(&self.raw_data, addr, num_chunks)
         }
+    }
+
+    pub fn into_inner(self) -> Box<[u32]> {
+        self.raw_data
     }
 }
 
 impl<'a> Timestep<'a> {
-    fn new(raw_data: &'a [u32], addr: u32) -> Result<Self, ()> {
+    fn new(raw_data: &'a [u32], addr: u32, num_chunks: u32) -> Result<Self, ()> {
         let byteslice = get_u8_slice(raw_data.get(addr as usize..).ok_or(())?);
 
         let smallest_symbol = byteslice[0] as i8;
@@ -105,17 +138,21 @@ impl<'a> Timestep<'a> {
         // that it is not unnecessarily coupled to this specific application. We convert
         // symbols between `u8` and `i8` as we pass them to or from the compression
         // module. On the machine code level, this conversion is a no-op.
+        let distribution = DistributionU8::new(smallest_symbol as u8, frequencies);
+
+        let start_addr = addr as usize + (frequencies_end + 3) / 4;
+        let chunk_addresses = raw_data
+            .get(start_addr..start_addr + num_chunks as usize)
+            .ok_or(())?;
 
         Ok(Timestep {
-            distribution: DistributionU8::new(smallest_symbol as u8, frequencies),
-            chunk_addresses: raw_data
-                .get((addr as usize + (frequencies_end + 3) / 4)..)
-                .ok_or(())?,
+            distribution,
+            chunk_addresses,
             raw_data,
         })
     }
 
-    fn chunk<'s>(&'s self, index: u32) -> Result<Decoder<'s, 'a>, ()> {
+    pub fn chunk<'s>(&'s self, index: u32) -> Result<Decoder<'s, 'a>, ()> {
         let addr = *self.chunk_addresses.get(index as usize).ok_or(())?;
         let compressed_data = get_u16_slice(self.raw_data.get(addr as usize..).ok_or(())?);
         self.distribution.decoder(compressed_data)
@@ -152,7 +189,7 @@ mod test {
 
     #[test]
     fn construct_file() {
-        let mut data = vec![
+        let data = vec![
             255, // magic
             0,   // major_version
             0,   // minor_version
@@ -166,11 +203,11 @@ mod test {
             4, 5, 6, // embedding vectors of last time step (4 at a time given as `u32`s)
         ];
 
-        let file = EmbeddingFile::new(&data).unwrap();
-        assert_eq!(file.header.file_size, 15);
-        assert_eq!(file.timestep_addrs, [10]);
+        let file = EmbeddingFile::new(data.into_boxed_slice()).unwrap();
+        assert_eq!(file.header().file_size, 15);
 
+        let mut data = file.into_inner();
         data[3] = 11; // Invalidate the file size field.
-        assert!(EmbeddingFile::new(&data).is_err());
+        assert!(EmbeddingFile::new(data).is_err());
     }
 }
