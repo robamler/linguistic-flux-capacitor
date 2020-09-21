a09aff26abe6366109d6eb3737533022e45072a0
robamler <robamler@users.noreply.github.com>
2020-09-21 23:20:42 +0000

    deploy: 3890039c7c9cf895032986feeb4524c419a53a24

--- main.js
+++ main.js
@@ -171,7 +171,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/word_history_explorer_backend_bg.wasm":"6a1b5d469f71653ff840"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/word_history_explorer_backend_bg.wasm":"dca38eadc93ce9dadcac"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
