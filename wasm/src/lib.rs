28659df1ce71ee6b8ad15f6fad1860a4dadd7666
Robert Bamler <robert.bamler@uni-tuebingen.de>
2022-12-30 14:19:41 +0100

    Move from `wee_alloc` to `lol_alloc`
    Since `wee_alloc` seems to be unmaintained.

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,8 +1,10 @@
 pub mod embedding_file_builder;
 pub mod utils;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
+#[cfg(target_arch = "wasm32")]
+use lol_alloc::{FreeListAllocator, LockedAllocator};
+
+#[cfg(target_arch = "wasm32")] // Don't use `lol_alloc` for test builds.
 #[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+static ALLOCATOR: LockedAllocator<FreeListAllocator> =
+    LockedAllocator::new(FreeListAllocator::new());
