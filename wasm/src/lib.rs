8c6c11fcbfc76116a8a498be802ea8f6dca07cea
Robert Bamler <robert.bamler@uni-tuebingen.de>
2025-02-11 17:28:27 +0100

    Remove specialized allocator
    Rust's standard library now seems to contain a (reasonably small)
    wasm-compatible heap memory allocator. We're switching to this std
    allocator since externally implemented allocators are still a pain to
    use and maintain.

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,10 +1,2 @@
 pub mod embedding_file_builder;
 pub mod utils;
-
-#[cfg(target_arch = "wasm32")]
-use lol_alloc::{FreeListAllocator, LockedAllocator};
-
-#[cfg(target_arch = "wasm32")] // Don't use `lol_alloc` for test builds.
-#[global_allocator]
-static ALLOCATOR: LockedAllocator<FreeListAllocator> =
-    LockedAllocator::new(FreeListAllocator::new());
